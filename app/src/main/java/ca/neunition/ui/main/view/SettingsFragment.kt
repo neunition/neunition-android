1016781464fb5b07cfc173ea4526b690be53ed8d
Nelaven Subaskaran <nsubaska@uwo.ca>
2022-09-09 23:03:43 -0400

    Update SettingsFragment

--- app/src/main/java/ca/neunition/ui/main/view/SettingsFragment.kt
+++ app/src/main/java/ca/neunition/ui/main/view/SettingsFragment.kt
@@ -15,7 +15,6 @@ import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
